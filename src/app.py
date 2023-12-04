4e4bf031222057ba6020ecb555b565cc4cd81007
giovanni.valentini <valentini.giovanni96@gmail.com>
2023-12-04 19:24:53 +0100

    added record and fixes

--- src/app.py
+++ src/app.py
@@ -30,8 +30,8 @@ def display_lineplot(df):
     sorted_filtered_games = df.sort_values('id')
 
     # Plotting the points
-    ax.plot(sorted_filtered_games['id'], sorted_filtered_games['points_gsqsa'], label='Points GSQSA', color='blue')
-    ax.plot(sorted_filtered_games['id'], sorted_filtered_games['points_opponent'], label='Points Opponent', color='orange')
+    ax.plot(sorted_filtered_games['id'], sorted_filtered_games['points_gsqsa'], label='Points GSQSA', color='green')
+    ax.plot(sorted_filtered_games['id'], sorted_filtered_games['points_opponent'], label='Points Opponent', color='red')
 
     # Adding labels and title
     ax.set_xlabel('Game ID')
@@ -47,13 +47,13 @@ games = load_data()
 styled_games = style_games(games.set_index("id"))
 
 # Title of the app
-st.title("GSQSA Basket Team Stats 2023/2024")
+st.title("GSQSA Basket Stats 2023/2024 🏀")
 
 # Show the dataframe without the index
 st.markdown(styled_games.to_html(index=True), unsafe_allow_html=True)
 
 # KPI calculation
-st.subheader("Averages")
+st.subheader("Stats")
 
 # User selection for filtering
 filter_option_home = st.selectbox(
@@ -83,8 +83,6 @@ max_id = selected_games['id'].max()
 
 # Apply filters to the DataFrame
 filtered_games = games[(games['id'] >= min_id) & (games['id'] <= max_id)]
-
-
 #filtered_games = games
 
 if filter_option_home != "All Games":
@@ -95,21 +93,28 @@ if filter_option_win != "All Games":
     win_value = 1 if filter_option_win == "Wins" else 0
     filtered_games = filtered_games[filtered_games["win"] == win_value]
 
+# Calculate win-loss record
+num_wins = filtered_games["win"].sum()
+num_losses = len(filtered_games) - num_wins
+record = f"{num_wins}-{num_losses}"
+
 # Calculate the averages
 avg_points_gsqsa = filtered_games["points_gsqsa"].mean()
 avg_points_opponent = filtered_games["points_opponent"].mean()
 
 # Create columns for each KPI
+record_col = st.columns(1)
 col1, col2 = st.columns(2)
 
 # Custom HTML and CSS for KPIs
-kpi1_html = f"<div style='text-align: center; color: green;'><span style='font-size: 1.2em;'>Average Points GSQSA</span><br><span style='font-size: 2.5em;'>{avg_points_gsqsa:.2f}</span></div>"
+record_html = f"<div style='text-align: center; color: #FFA500;'><span style='font-size: 1.2em;'>Win-Loss Record</span><br><span style='font-size: 2.5em;'>{record}</span></div>"
+kpi1_html = f"<div style='text-align: center; color: #33FF49;'><span style='font-size: 1.2em;'>Average Points GSQSA</span><br><span style='font-size: 2.5em;'>{avg_points_gsqsa:.2f}</span></div>"
 kpi2_html = f"<div style='text-align: center; color: red;'><span style='font-size: 1.2em;'>Average Points Opponent</span><br><span style='font-size: 2.5em;'>{avg_points_opponent:.2f}</span></div>"
 
 # Display the KPIs in their respective columns
+record_col[0].markdown(record_html, unsafe_allow_html=True)
 col1.markdown(kpi1_html, unsafe_allow_html=True)
 col2.markdown(kpi2_html, unsafe_allow_html=True)
 
 # display line plot
 display_lineplot(filtered_games)
-
